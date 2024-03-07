5c3cca0e3530a080c83ea5c31c352dae29be3d02
DevinJM3 <106412270+DevinJM3@users.noreply.github.com>
2024-03-06 23:14:21 -0600

    Checkpoint 1

--- src/block_store.c
+++ src/block_store.c
@@ -1,8 +1,13 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdint.h>
 #include "bitmap.h"
 #include "block_store.h"
 // include more if you need
+typedef struct{
+    bitmap_t *bitmap;
+    uint8_t data[BLOCK_STORE_NUM_BLOCKS][BLOCK_SIZE_BYTES];
+}block_store;
 
 // You might find this handy.  I put it around unused parameters, but you should
 // remove it before you submit. Just allows things to compile initially.
@@ -10,12 +15,26 @@
 
 block_store_t *block_store_create()
 {
-    return NULL;
+    block_store *bs = calloc(1, sizeof(block_store));   // Allocate the blockstore with 0 as the data
+    if(bs == NULL)
+        return NULL;
+
+    bs->bitmap = bitmap_overlay(BLOCK_SIZE_BYTES * 8,  bs->data[BITMAP_START_BLOCK]);
+    if(bs->bitmap == NULL){
+        free(bs);
+        return NULL;
+    }
+    
+    return (block_store_t*)bs;
 }
 
 void block_store_destroy(block_store_t *const bs)
 {
-    UNUSED(bs);
+    if(bs == NULL)
+        return;
+
+    bitmap_destroy(((block_store*)bs)->bitmap); // Free the bitmap
+    free(bs);                                   // Free the block_store
 }
 size_t block_store_allocate(block_store_t *const bs)
 {
